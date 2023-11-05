ccad94c9900c874b2e1fe3d85b7da50745b83cb2
PudparK <p.barron@outlook.com>
2023-11-04 23:21:02 -0500

    first commit

---
+++ src/pages/movies/index.tsx
@@ -0,0 +1,16 @@
+// import "../../app/globals.css";
+
+export default function Movies() {
+  return (
+    <>
+      <title>Movie Page</title>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+          <h1 className="text-3xl font-bold underline text-green-500">
+            Hello world!
+          </h1>
+        </div>
+      </main>
+    </>
+  );
+}
