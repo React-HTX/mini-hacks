5c58a609bafa3069294884c6a39ec476d11aadbc
Tracy Tucker <tracytuckeremail@gmail.com>
2024-02-14 11:28:11 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from 'react-router-dom';
-import NotFound from './notFound'
-import Home from './Home';
-import Navigation from './Navigation';
-import SortPage from './sortPage';
-import MovieDetailsPage from './movieDetailsPage';
-import SearchPage from './searchPage';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sortPage" element={<SortPage />} />
-        <Route path="/movies/:id" element={<MovieDetailsPage />} />
-        <Route path="/movies/search" element={<SearchPage />} />
+    // <Router>
+    //   <Navigation />
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/sortPage" element={<SortPage />} />
+    //     <Route path="/movies/:id" element={<MovieDetailsPage />} />
+    //     <Route path="/movies/search" element={<SearchPage />} />
 
-        <Route path="*" element={<NotFound />} />
-        {/* Define more routes here */}
-      </Routes>
-    </Router>
+    //     <Route path="*" element={<NotFound />} />
+    //   </Routes>
+    // </Router>
+    Testing
   );
 }
 
-
 export default App;
-
--- src/Contact.js
+++
@@ -1,5 +0,0 @@
-function Contact() {
-    return <h1 className="text-3xl font-bold underline">Contact Page</h1>;
-}
-
-export default Contact;
