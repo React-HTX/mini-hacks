0331653fc0df2b4d4ef47c50392f829c3f3edf24
Tracy Tucker <tracytuckeremail@gmail.com>
2024-02-14 11:43:38 -0600

    Removes react-router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    // <Router>
-    //   <Navigation />
-    //   <Routes>
-    //     <Route path="/" element={<Home />} />
-    //     <Route path="/sortPage" element={<SortPage />} />
-    //     <Route path="/movies/:id" element={<MovieDetailsPage />} />
-    //     <Route path="/movies/search" element={<SearchPage />} />
-
-    //     <Route path="*" element={<NotFound />} />
-    //   </Routes>
-    // </Router>
-    Testing
-  );
+  return <div>testing</div>;
 }
 
 export default App;
