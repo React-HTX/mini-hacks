bdfe79ce236b6f28a9f11fc21bde2fc03379860e
Tracy Tucker <tracytuckeremail@gmail.com>
2024-02-14 18:42:22 -0600

    Adds button logic and log

--- challenges/src/components/One.jsx
+++ challenges/src/components/One.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const One = () => {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+    console.log(count);
+  }
+
   return (
     <main>
       <div className="section">
         <div className="section-center">
-          <h2>Add State</h2>
+          <h2>Log Correct State</h2>
+          <p>
+            Open your console, then click on the button to log state. Is the
+            result what you expected?
+            <br />
+            Correct the logic in your code.
+          </p>
+        </div>
+        <div className="section-center">
+          <button className="btn" type="button" onClick={handleClick}>
+            You pressed me {count} times
+          </button>
         </div>
       </div>
     </main>
