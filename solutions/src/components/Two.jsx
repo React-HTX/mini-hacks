92ec0e64c435d16f2253685d17d8c1ac78c10a8d
Tracy Tucker <tracytuckeremail@gmail.com>
2024-02-18 08:08:27 -0600

    Adds answers for problem 2

--- solutions/src/components/Two.jsx
+++ solutions/src/components/Two.jsx
@@ -3,10 +3,31 @@ import React, { useState } from 'react';
 const Two = () => {
   const [age, setAge] = useState(12);
 
+  /*
+  Wrong way!
   function handleClick() {
     setAge(age + 1);
     setAge(age + 1);
     setAge(age + 1);
+  }
+  */
+
+  /*
+  Think of the above code as this:
+  function handleClick() {
+    setAge(12 + 1);
+    setAge(12 + 1);
+    setAge(12 + 1);
+  }
+  */
+
+  // Correct way!
+  function handleClick() {
+    // prevAge --> UPDATER FUNCTION
+    // (a) => {} --> Another naming convention
+    setAge((prevAge) => prevAge + 1);
+    setAge((prevAge) => prevAge + 1);
+    setAge((prevAge) => prevAge + 1);
   }
 
   return (
@@ -35,3 +56,26 @@ const Two = () => {
 };
 
 export default Two;
+
+/*
+On problem One, we learned that setting state only updates state on the next render.
+React only gives you a snapshot of the state for that render.
+HOWEVER, React provides a way to get the previous state before the update:
+UPDATE STATE USING PREVIOUS STATE
+When you need to update state based on the previous state, use the function form of the setState method.
+The function form of setState receives the previous state as an argument.
+This way, you can access the previous state and update it accordingly.
+***When you set a function as the argument of the setState method, React will pass the previous state to that function.***
+
+
+*/
+/*
+3rd option
+function handleClick() {
+  setCount(count + 1);
+  setCount((count) => {
+    console.log(count);
+    return count;
+  });
+}
+*/
