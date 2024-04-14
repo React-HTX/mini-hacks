0fe8a6f8a32edd1e70ceabad31cf70d451e5de79
MyNameIsJohnson <johnsonle2929@gmail.com>
2024-04-14 18:12:46 -0500

    Changed headings order

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -26,10 +26,10 @@ const Home = () => {
 
   return (
     <main className="flex bg-white min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-black text-2xl font-semibold">Top Trending Movies</h1>
       <h2 className="text-black text-xl font-semibold">
         Selected Title: {selectedTitle}
       </h2>
-      <h1 className="text-black text-2xl font-semibold">Top Trending Movies</h1>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {movies.map((movie) => (
