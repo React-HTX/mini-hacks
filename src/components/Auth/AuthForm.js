f9480456e8512ff136086af9c3ee504938f87b5c
MyNameIsJohnson <johnsonle2929@gmail.com>
2024-04-14 15:30:35 -0500

    Added AuthForm

---
+++ src/components/Auth/AuthForm.js
@@ -0,0 +1,114 @@
+import { useState, useRef, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+import AuthContext from "../../utils/auth-context";
+import classes from "./AuthForm.module.css";
+
+const AuthForm = () => {
+  const history = useNavigate();
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  const authCtx = useContext(AuthContext);
+  const [error, setError] = useState();
+
+  const [isLogin, setIsLogin] = useState(true);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    // can add validation here
+
+    setIsLoading(true);
+    let url;
+
+    console.log(process.env.REACT_APP_SIGNUP_API_KEY);
+    console.log(process.env.REACT_APP_SIGNINWITHPASSWORD_API_KEY);
+    if (isLogin) {
+      url = `${process.env.REACT_APP_SIGNINWITHPASSWORD_API_KEY}`;
+    } else {
+      url = `${process.env.REACT_APP_SIGNUP_API_KEY}`;
+      // can build custom hook here, but code below is a direct request without redux, hooks ...
+      // Need to use .env file to hide API key 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_Key]'
+    }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        setIsLoading(false);
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            // let errorMessage = "Authentication Failed!!!";
+            // if (data && data.error && data.error.message) {
+            //   setError(data.error.message);
+            // }
+            throw new Error(data.error.message);
+          });
+        }
+      })
+      .then((data) => {
+        alert("Success");
+        const expirationTime = new Date(
+          new Date().getTime() + +data.expiresIn * 1000
+        );
+        authCtx.login(data.idToken, expirationTime.toISOString());
+        history.replace("/");
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
+  };
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordInputRef}
+          />
+          {error}
+        </div>
+        <div className={classes.actions}>
+          {!isLoading && (
+            <button>{isLogin ? "Login" : "Create Account"}</button>
+          )}
+          {isLoading && <p>Sending Request</p>}
+          <button
+            type="button"
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? "Create new account" : "Login with existing account"}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
