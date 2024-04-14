3938125fc2a0b64524e6559a4ff745ff7553af7d
MyNameIsJohnson <johnsonle2929@gmail.com>
2024-04-14 13:54:10 -0500

    Rearranged components and pages

---
+++ src/pages/Contact.js
@@ -0,0 +1,5 @@
+function Contact() {
+    return <h1 className="text-3xl font-bold underline">Contact Page</h1>;
+}
+
+export default Contact;
---
+++ src/pages/SearchPage.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import SearchResults from "./pages/SearchResults";
+import { getMovies } from "../utils/request";
+
+function SearchPage() {
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get("query");
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      if (query) {
+        const fetchedMovies = await getMovies(query);
+        setMovies(fetchedMovies);
+      }
+    };
+
+    fetchMovies();
+  }, [query]);
+
+  return <SearchResults searchText={query} movies={movies} />;
+}
+
+export default SearchPage;
