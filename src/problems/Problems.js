dcf957b1f78d491e4fecedc0e86482f359f08320
MyNameIsJohnson <johnsonle2929@gmail.com>
2024-04-23 17:54:36 -0500

    Removed unused imports

--- src/problems/Problems.js
+++ src/problems/Problems.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import ProblemOne from "./ProblemOne";
-import ProblemTwo from "./ProblemTwo";
 
 function Problems() {
   return (
