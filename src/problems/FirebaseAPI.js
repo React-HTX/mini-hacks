3a3586caf63ef4d0060d3830da971ff62b5822c0
MyNameIsJohnson <johnsonle2929@gmail.com>
2024-06-23 15:01:53 -0500

    updated tailwind

--- src/problems/FirebaseAPI.js
+++ src/problems/FirebaseAPI.js
@@ -2,22 +2,15 @@ import React from "react";
 
 const FirebaseAPI = () => {
   return (
-    <div className="problems ">
-      <h1>Firebase API</h1>
-      <div>
-        <div>
-          <p>
-            This is will pretty straight forward. All we need to do with your
-            API Key is place it in the ".env" file. Open your ".env" and
-            replace,
-            <span> "add-your-web-api-key-here" </span>
-            with your API Key.
-          </p>
-        </div>
-        <div>
-          <img src={process.env.PUBLIC_URL + "/env.png"} alt="" />
-        </div>
-      </div>
+    <div className="flex min-h-screen flex-col items-center py-10 px-60">
+      <h1 className="text-black text-4xl font-semibold p-10">Firebase API</h1>
+      <p className="font-semibold py-10 text-2xl w-[30em] text-justify">
+        This is will pretty straight forward. All we need to do with your API
+        Key is place it in the ".env" file. Open your ".env" and replace,
+        <span> "add-your-web-api-key-here" </span>
+        with your API Key.
+      </p>
+      <img src={process.env.PUBLIC_URL + "/env.png"} alt="" />
     </div>
   );
 };
